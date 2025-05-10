64ddac81d2aaf98092e84bf6f4a84c4b3608e47f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-11 04:31:42 +0600

    update

--- LeetCode/1.TwoSum.cpp
+++ LeetCode/1.TwoSum.cpp
@@ -1,22 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> twosum(vector<int> &nums, int target) {
+vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int> hashTable;
     for(int i=0; i<nums.size(); i++) {
-        int key = target - nums[i]; //variable increase complexity
+        // find the vlaue whice one need to search 
+        int key = target - nums[i];
+        // if the key found in hashTable
         if(hashTable.count(key)) {
+            // return the value of that key and index ofanother vlaue
             return {hashTable[key], i};
         }
+        // insert key-value(number and it's index) pairto the hashTable
         hashTable[nums[i]] = i;
     }
-    return {};
+    // return an empty array if the value not fuound
+    return{};
 }
 
 int main() {
 
     vector<int> num = {2, 7, 11, 15};
-    vector<int> res = twosum(num, 9);
+    vector<int> res = twoSum(num, 9);
     cout << res[0] << " " << res[1] << endl;
 
     return 0;
--- LeetCode/238.ProductofArrayExceptSelf.cpp
+++ LeetCode/238.ProductofArrayExceptSelf.cpp
@@ -2,22 +2,15 @@
 using namespace std;
 
 vector<int> productExceptSelf(vector<int>& nums) {
-    vector<int> a, b, c;
-        for(int i=1; i<nums.size()+1; i++) {
-            a.push_back(nums[i]*nums[i+1]);
-        }
-        for(int i=nums.size()-1; i>0; i--) {
-            b.push_back(nums[i]*nums[i-1]);
-        }
-        for(int i=0; i<nums.size(); i++) {
-            c.push_back(a[i]*b[i]);
-        }
-    return c;
+    vector<int> c;
+    
+    return nums;
 }
 
 int main() {
 
-    vector<int> nums = {1,2,3,4};
+    vector<int> nums(5,1);
+    // vector<int> nums = {1,2,3,4};
     vector<int> answer = productExceptSelf(nums);
     for(int i=0; i<answer.size(); i++) {
         cout << answer[i] << " ";
