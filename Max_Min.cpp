6050bac143f8e2c7a532da81c213650bc5a562c7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-23 23:59:20 +0600

    sliding window

--- Max_Min.cpp
+++ Max_Min.cpp
@@ -1,3 +1,6 @@
+// HackerRank Problem: n সংখ্যাক array element থেকে k সংখ্যাক element নিয়ে নতুন array তৈরি করে সেই নতুন array থেকে max-min করতে হবে। এভাবে, মূল array তে সর্বনিম্ন যে মান পাওয়া যাবে তা কাঙ্খিত সমাধান।
+//Learning: sliding window pattern
+
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
@@ -13,7 +16,7 @@ int main() {
     sort(arr.begin(), arr.end());
     for(int i=0; i+k-1<arr.size(); i++) {
         mini = min(mini, arr[i+k-1]-arr[i]);
-    }
+    } //sliding window approach applied
 
     cout << mini << endl;
 
