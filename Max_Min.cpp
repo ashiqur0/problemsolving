8532c78394116ac89b0f05d87f11986a4b3cfe40
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-23 23:46:15 +0600

    Sliding window approach

--- Max_Min.cpp
+++ Max_Min.cpp
@@ -2,27 +2,20 @@
 using namespace std;
 int main() {
 
-    int n, k, result = 0;
+    int n, k, mini = INT_MAX;
     cin >> n >> k;
-    vector<int> arr(n);
-    vector<int> arr2(k);
 
+    vector<int> arr(n);    
     for(int i=0; i<n; i++) {
         cin >> arr.at(i);
     }
     
-    // sort(arr.begin(), arr.end());
-
-    for(int i=0; i<n-k; i++) {
-        for(int j=0; j<k; j++) {
-            arr2.at(j) = arr.at(j);
-        }
-        auto mx = max_element(arr2.begin(), arr2.end());
-        auto mn = min_element(arr2.begin(), arr2.end());
-        result = max(result, mx - mn);
+    sort(arr.begin(), arr.end());
+    for(int i=0; i+k-1<arr.size(); i++) {
+        mini = min(mini, arr[i+k-1]-arr[i]);
     }
 
-    cout << result << endl;
+    cout << mini << endl;
 
     return 0;
 }
