d08e9f8e129045eee348de20bf0cd0ca9fdd46ad
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-24 00:29:46 +0600

    Disting Pair Count

--- A_Indian_Summer.cpp
+++ A_Indian_Summer.cpp
@@ -1,6 +1,22 @@
 // CodeForces Problem: Distink pair count করতে হবে।
 // Learning: pair, set
+/*
+Input 1:
+5
+birch yellow
+maple red
+birch yellow
+maple yellow
+maple green
+Optput 1: 4
 
+Input 2:
+3
+oak yellow
+oak yellow
+oak yellow
+Optput 2: 1
+*/
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
