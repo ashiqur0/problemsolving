fe05a7681741148e44f7c6f8f16a42f712152acc
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-29 23:36:49 +0600

    Codeforces Problem

--- A_Treasure_Hunt.cpp
+++ A_Treasure_Hunt.cpp
@@ -5,26 +5,9 @@ int main() {
     int t;
     cin >> t;
     for(int i=0; i<t; i++) {
-        int x, y, a, dig=0;
+        int x, y, a, d=0;
         cin >> x >> y >> a;
-
-        bool littleB = true;
-
-        while(dig<=a) {
-            if (littleB){
-                dig+=x;
-                if(dig>a) {
-                    cout << "NO" << endl;
-                }
-                littleB = false;
-            } else {
-                dig+=y;
-                if(dig>a) {
-                    cout << "YES" << endl;
-                }
-                littleB = true;
-            }
-        }
+        (a % (x+y) < x)? cout << "NO\n" : cout << "YES\n" ;
     }
 
   return 0;
