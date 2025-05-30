d51ea55942bc611021ddac692efc0e83a7f0dc2c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-30 06:30:51 +0600

    Codeforces Problem

--- A_Ideal_Generator.cpp
+++ A_Ideal_Generator.cpp
@@ -2,16 +2,12 @@
 using namespace std;
 int main() {
 
-    int n;
-    cin >> n;
-    for(int i=0; i<n; i++) {
+    int t;
+    cin >> t;
+    while(t--) {
         int a;
         cin >> a;
-        if(a%2==0) {
-            cout << "NO" << endl;
-        } else {
-            cout << "YES" << endl;
-        }
+        (a%2==0) ? cout << "NO\n" : cout << "YES\n";
     }
 
   return 0;
