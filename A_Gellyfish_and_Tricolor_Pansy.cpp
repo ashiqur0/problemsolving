d649095d76726d624de0a3434b7600b1f8765db1
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-05-31 22:09:10 +0600

    codeforces contest

---
+++ A_Gellyfish_and_Tricolor_Pansy.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        
+        int m = min(min(a, b), min(c, d));
+        if(m==a && a!=b){
+            cout<< "Flower\n";
+        }else if(m==c && c!=d){
+            cout<< "Flower\n";
+        }else{
+            cout<< "Gellyfish\n";
+        }
+    }
+
+  return 0;
+}
