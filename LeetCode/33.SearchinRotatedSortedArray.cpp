4948f135a0d31c8341f32f59a3c8cfbf678ff28f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-01 22:24:22 +0600

    Leetcode problem

--- LeetCode/33.SearchinRotatedSortedArray.cpp
+++ LeetCode/33.SearchinRotatedSortedArray.cpp
@@ -2,32 +2,24 @@
 using namespace std;
 
 // Modified Binary Search
-int binarySearch(vector<int>& v, int target) {
-    int start = 0, end = v.size() - 1;
-    
-    while(start <= end){
+int binarySearch(vector<int>& nums, int target) {
+    int start = 0, end = nums.size() - 1;
+    while(start <= end) {
         int mid = (start + end) / 2;
+        if(nums[mid] == target) return mid;
 
-        if(v[mid] == target) {
-            return mid;
-        }
-
-        if(v[start] <= v[mid]) { //assume left part is sorted
-            if(target >= v[start] && target < v[mid]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        } else {                // right part is sorted
-            if(target <= v[end] && target > v[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+        if(nums[start] <= nums[mid]) { //left part of the array is sorted
+            if(target >= nums[start] && target < nums[mid]) end = mid - 1;
+            else start = mid + 1;
+        } else {                       //right part is sorted
+            if(target <= nums[end] && target > nums[mid]) start = mid + 1;
+            else end = mid - 1;
         }
     }
+
     return -1;
 }
+
 int main() {
 
     // vector<int> v = {4,5,6,7,0,1,2};
@@ -36,10 +28,10 @@ int main() {
     // int target = 3;
     // vector<int> v = {1};
     // int target = 0;
-    vector<int> v = {4,5,6,7,8,1,2,3};
+    vector<int> nums = {4,5,6,7,8,1,2,3};
     int target = 8;
 
-    cout << binarySearch(v, target) << endl;
+    cout << binarySearch(nums, target) << endl;
 
     return 0;
 }
