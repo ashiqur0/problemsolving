dcacbb88d81b424d35f7fd2156fe21512df53824
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-02 23:23:20 +0600

    LeetCode: 15. 3Sum

--- LeetCode/15.3Sum.cpp
+++ LeetCode/15.3Sum.cpp
@@ -1,3 +1,6 @@
+// LeetCode: 15. 3Sum
+// Learning: Modified 2 sum, Two Pointer approach
+// Description: Given an integer array. Have to return a 2D array contain three element whose sum = 0.
 #include <bits/stdc++.h>
 using namespace std;
 // Approach 1 - Brute Force
@@ -23,32 +26,6 @@ using namespace std;
 // }
 
 // Approach 2 - Modified 2 sum
-// vector<vector<int>> threeSum(vector<int>& nums) {
-//     sort(nums.begin(), nums.end());
-//     vector<vector<int>> ans;
-//     for (int i = 0; i < nums.size(); i++) {
-//         if (i > 0 && nums[i] == nums[i - 1]) {
-//             continue;
-//         }
-//         int j = i + 1;
-//         int k = nums.size() - 1;
-//         while (j < k) {
-//             if (nums[i] + nums[j] + nums[k] == 0) {
-//                 ans.push_back({nums[i], nums[j], nums[k]});
-//                 while (j < k && nums[j] == nums[j + 1]) j++;
-//                 while (j < k && nums[k] == nums[k - 1]) k--;
-//                 j++;
-//                 k--;
-//             } else if (nums[i] + nums[j] + nums[k] > 0) {
-//                 k--;
-//             } else {
-//                 j++;
-//             }
-//         }
-//     }
-//     return ans;
-// }
-
 vector<vector<int>> threeSum(vector<int>& nums) {
     vector<vector<int>> res;
     sort(nums.begin(), nums.end());
