cc8fbd79285b8a3a56ded9eb58bad42fc47574ea
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 00:11:32 +0600

    191. Number of 1 Bits

--- LeetCode/191.Numberof1Bits.cpp
+++ LeetCode/191.Numberof1Bits.cpp
@@ -1,12 +1,15 @@
+// LeetCode: 191. Number of 1 Bits
+// Learning: Binary Digit
+// Description: Given a positive number. Count how many digit of its equivalent binary number is 1; 
 #include <bits/stdc++.h>
 using namespace std;
 int hammingWeight(int n) {
     int count = 0;
-    while(n != 0) {
-        if(n&1==1) {
+    while(n) {
+        if(n%2 == 1) {
             count++;
-            n >> 1;
         }
+        n/=2;
     }
     return count;
 }
