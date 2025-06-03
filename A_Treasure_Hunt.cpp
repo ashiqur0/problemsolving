fb1c23c3614bb144d83dd536ccee33ba07dd24da
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-03 10:01:27 +0600

    CodeForces: A. Treasure Hunt

--- A_Treasure_Hunt.cpp
+++ A_Treasure_Hunt.cpp
@@ -1,6 +1,6 @@
 // CodeForces: A. Treasure Hunt
 // Learning: Implementation, Math
-// Description: B and K digging Treasure. Each day B dig x metter, K dig y metter. The Treasure is a meeter. Who will get the treasure first? If B dig more than the a first print YES, and if K dig more than a print NO
+// Description: B and K digging Treasure. Each day B dig x metter, K dig y metter. The Treasure is located in a meeter deep. Who will get the treasure first? If B dig more than the a metter then B will get the treasure first. In that case print "YES", and if K dig more than a meeter first then K will get the treasuere first. In that case print "NO".
 
 #include <iostream>
 using namespace std;
