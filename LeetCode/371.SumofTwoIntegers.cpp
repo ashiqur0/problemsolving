d17d417202ad0dae2d75caf779777e756a8c6a70
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-03 23:47:16 +0600

    LeetCode: 371. Sum of Two Integers

--- LeetCode/371.SumofTwoIntegers.cpp
+++ LeetCode/371.SumofTwoIntegers.cpp
@@ -1,3 +1,10 @@
+// LeetCode: 371. Sum of Two Integers
+// Learning: Bit Manipulation; 
+            // Bitwise AND (a&b), 
+            // Bitwise XOR (a^b), 
+            // Left Shift Operator (<<)
+// Description: Given two numbers. Calculate their sum without usign + or - operator
+
 #include <bits/stdc++.h>
 using namespace std;
 int getSum(int a, int b) {
