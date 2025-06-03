1c540f6764b1cec8f9d6e8a9fde071a4b8a07147
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-03 23:25:09 +0600

    A. Even Odds

---
+++ A_Even_Odds.cpp
@@ -0,0 +1,16 @@
+// CodeForces: A. Even Odds
+// Learning: Math
+// Description: Given n and k. 1 to till n the odd number will be stored sequentially then 1 to till n even number will be stored. Need to print the k positioned number.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    long long int n, k;
+    cin >> n >> k;
+
+    long long int p = ceil(n/2.0);
+    cout << (p >= k? k*2-1 : (k-p)*2) << endl;
+
+  return 0;
+}
