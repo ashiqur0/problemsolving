6edc79d7b11558ef0431744176ec7516285972ea
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-04 11:06:35 +0600

    LeetCode: 268. Missing Number

--- LeetCode/268.MissingNumber.cpp
+++ LeetCode/268.MissingNumber.cpp
@@ -1,12 +1,22 @@
+// LeetCode: 268. Missing Number
+// Learning: Bit Manipulation;
+// Description: Given an array of elements range [0-n]. Find the missing number
+
 #include <bits/stdc++.h>
 using namespace std;
 int missingNumber(vector<int>& nums) {
-    
+    int n = nums.size();
+    int total = n*(n+1)/2;
+    int sum = 0;
+    for(const int &i : nums) {
+        sum += i;
+    }
+    return total - sum;
 }
 int main() {
-    vector<int> v = {3,0,1};    //2
+    // vector<int> v = {3,0,1};    //2
     // vector<int> v = {0,1};   //2
-    // vector<int> v = {9,6,4,2,3,5,7,0,1}; //8
+    vector<int> v = {9,6,4,2,3,5,7,0,1}; //8
     cout << missingNumber(v) << endl;
 
   return 0;
