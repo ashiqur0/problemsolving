dec1786f2817b68b2013861dc3ce6827aa1e91bf
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-11 17:31:07 +0600

    LeetCode: 139. Word Break

--- LeetCode/Dynamic_Programming/139.WordBreak.cpp
+++ LeetCode/Dynamic_Programming/139.WordBreak.cpp
@@ -1,7 +1,8 @@
-// LeetCode: 139. Word Break
-// Learning: Array, Hash Table, String, Dynamic Programming, Trie, Memoization
-// Description: Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words. Note that the same word in the dictionary may be reused multiple times in the segmentation.
-
+/*
+LeetCode: 139. Word Break
+Learning: Array, Hash Table, String, Dynamic Programming, Trie, Memoization
+Description: Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words. Note that the same word in the dictionary may be reused multiple times in the segmentation.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
