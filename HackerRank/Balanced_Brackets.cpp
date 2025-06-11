2f4b4c32d7a521cffc0e3baa9b84ff1f991e0506
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-11 16:32:42 +0600

    HackerRank: Balanced Brackets

--- HackerRank/Balanced_Brackets.cpp
+++ HackerRank/Balanced_Brackets.cpp
@@ -1,14 +1,39 @@
+/* 
+HackerRank: Balanced Brackets
+Learning: String, Stack
+Description: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is Balanced. An input string is Balanced if: 
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+Print Yes : if Balanced
+Print No : otherwise
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
-// char closing(char c) {
-//     if (c==')') return '(';
-//     else if (c=='}') return '{';
-//     else if (c==']') return '[';
-//     else if (c=='(') return ')';
-//     else if (c=='{') return '}';
-//     else if (c=='[') return ']';
-//     // return 0;
-// }
+
+bool isBalanced(string s) {
+    int n = s.size();
+    stack<char> stack;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stack.push(s[i]);
+        } else {
+            if (stack.empty()) {
+                return false;
+            }
+            char top = stack.top();
+            if ((s[i] == ')' && top != '(') || 
+                (s[i] == '}' && top != '{') || 
+                (s[i] == ']' && top != '[')) {
+                return false;
+            }
+            stack.pop();
+        }
+    }
+    return stack.empty();
+}
 
 int main() {    
 
@@ -17,22 +42,7 @@ int main() {
     while(t--) {
         string s;
         cin >> s;
-
-        stack<char> st;
-        for(int i=0; i<s.size(); i++) {
-            // st.top() == closing(s[i])? st.pop() : st.push(s[i]);
-            if(s[i] == ')' && st.top() == '(') {
-                st.pop();
-            } else if(s[i] == '}' && st.top() == '{') {
-                st.pop();
-            } else if(s[i] == ']' && st.top() == '[') {
-                st.pop();
-            } else {
-                st.push(s[i]);
-            }
-        }
-
-        st.empty()? cout << "YES" << endl : cout << "NO" << endl;
+        cout << (isBalanced(s)? "YES" : "NO") << endl;
     }
 
   return 0;
