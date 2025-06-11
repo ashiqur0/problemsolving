14bf2d834a686f2ff966c06b57d5407def027e41
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-11 16:45:23 +0600

    HackerRank: Max Min

--- HackerRank/Max_Min.cpp
+++ HackerRank/Max_Min.cpp
@@ -1,5 +1,8 @@
-// HackerRank Problem: n সংখ্যাক array element থেকে k সংখ্যাক element নিয়ে নতুন array তৈরি করে সেই নতুন array থেকে max-min করতে হবে। এভাবে, মূল array তে সর্বনিম্ন যে মান পাওয়া যাবে তা কাঙ্খিত সমাধান।
-//Learning: sliding window pattern
+/*
+HackerRank: Max Min
+Learning: sliding window pattern
+Description: n সংখ্যাক array element থেকে k সংখ্যাক element নিয়ে নতুন array তৈরি করে সেই নতুন array থেকে max-min করতে হবে। এভাবে, মূল array তে সর্বনিম্ন যে মান পাওয়া যাবে তা কাঙ্খিত সমাধান।
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
