b3b647426284c89789aaa3d667e4287e34b42bdb
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-11 16:52:36 +0600

    HackerRank: The Time in Words

--- HackerRank/The_Grid_Search.py
+++
@@ -1,6 +0,0 @@
-t = int(input())
-for i in range(t):
-    a = int(input())
-    b = int(input())
-    for j in range(a):
-        l[j] = input()
--- HackerRank/The_Time_in_Words.py
+++ HackerRank/The_Time_in_Words.py
@@ -1,5 +1,17 @@
-# Hacker Rank Mid range problem
-
+'''
+HackerRank: The Time in Words
+Learning: Math
+Description: Given the time in numerals we may convert it into words, as shown below:
+    5:00 five o' clock
+    5:01 one minute past five
+    5:10 ten minutes past five
+    5:15 quarter past five
+    5:30 half past five
+    5:40 → twenty minutes to six
+    5:45 quarter to six
+    5:47 thirteen minutes to six
+    5:28 twenty eight minutes past five
+'''
 h = int(input())
 m = int(input())
 
