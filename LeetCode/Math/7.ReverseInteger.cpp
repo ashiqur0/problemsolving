38aacb7d458ec79a8f355293b6368d6858f91466
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-13 20:47:43 +0600

    LeetCode: 7. Reverse Integer

---
+++ LeetCode/Math/7.ReverseInteger.cpp
@@ -0,0 +1,48 @@
+/*
+LeetCode: 7. Reverse Integer
+Learning: Math
+Description: Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int reverse(int x) {
+    int ans = 0;
+    while(x) {
+        int digit = x % 10;
+        x /= 10;
+        if(ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > 7)) return 0;
+        if(ans < INT_MIN / 10 || (ans == INT_MIN / 10 && digit < -8)) return 0;
+        ans = ans * 10 + digit;
+    }
+    return ans;
+}
+
+int main() {
+
+    // int n = 123;    // 321
+    // int n = -321;    // -123
+    // int n = 21;    // 12
+    int n = 1534236469;    // 0
+    cout << reverse(n) << endl;
+    // cout << INT_MAX << endl; //2147483647
+    cout << INT_MIN << endl;    //-2147483648
+
+  return 0;
+}
