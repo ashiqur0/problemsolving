a674f8fa16bc617f8b84922b7be7398399f70756
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-16 23:10:17 +0600

    CodeForces: D. 1D Eraser

--- CodeForces/D_1_D_Eraser.cpp
+++ CodeForces/D_1_D_Eraser.cpp
@@ -9,21 +9,11 @@ Description: You are given a strip of paper s
 Find the minimum number of operations needed to remove all black cells.
 
 Input
-The first line contains a single integer t
- (1≤t≤1000) — the number of test cases.
+The first line contains a single integer t (1≤t≤1000) — the number of test cases.
+The first line of each test case contains two integers n and k (1≤k≤n≤2⋅105) — the length of the paper and the integer used in the operation.
+The second line of each test case contains a string s of length n consisting of characters B (representing a black cell) or W (representing a white cell).
+The sum of n over all test cases does not exceed 2⋅105. 
 
-The first line of each test case contains two integers n
- and k
- (1≤k≤n≤2⋅105) — the length of the paper and the integer used in the operation.
-
-The second line of each test case contains a string s
- of length n
- consisting of characters B
- (representing a black cell) or W
- (representing a white cell).
-
-The sum of n
- over all test cases does not exceed 2⋅105.
 Output
 For each test case, output a single integer — the minimum number of operations needed to remove all black cells.
 */
