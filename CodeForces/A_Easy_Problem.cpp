87ae7615ea7b649f0a925cbf01ddca368ff7c38c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-20 11:05:53 +0600

    CodeForces: A. Easy Problem

---
+++ CodeForces/A_Easy_Problem.cpp
@@ -0,0 +1,28 @@
+/*
+CodeForces: A. Easy Problem
+Learning: brute force, math
+Description: Cube is given an integer n. She wants to know how many ordered pairs of positive integers (a, b) there are such that a = n b. Since Cube is not very good at math, please help her!
+
+Input
+The first line contains an integer t (1 ≤ t ≤99) - the number of test cases.
+
+The only line of each test case contains an integer n (2 < n < 100).
+
+Output
+For each test case, output the number of ordered pairs (a, b) on a new line.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        cout << n-1 << endl;
+    }
+
+  return 0;
+}
