1cd09b407b75564fb38af2b4c7727c1206542c9d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-28 10:05:43 +0600

    CodeForces: Data Type and Conditions - Problem A

---
+++ CodeForces/1_Data_Type_and_Conditions/A_Say_Hello_With_C.cpp
@@ -0,0 +1,27 @@
+/*
+CodeForces: Data Type and Conditions - Problem A
+Problem Name: A. Say Hello With C++
+Learning: Data Type, Input, Output,
+Description: Given a name S. Print "Hello, (name)" without parentheses.
+
+Input
+Only one line containing a string S.
+
+Output
+Print "Hello, " without quotes, then print name.
+
+Example: 
+Input : programmer
+Output: Hello, programmer
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string name;
+    cin >> name;
+    cout << "Hello, " << name << endl;
+
+  return 0;
+}
