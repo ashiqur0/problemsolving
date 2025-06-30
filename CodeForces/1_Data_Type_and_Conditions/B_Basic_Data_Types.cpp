e996851c388b7813cc4d2bb0b1efab1347090f32
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-30 06:45:22 +0600

    CodeForces: Data Type and Conditions - Problem B

---
+++ CodeForces/1_Data_Type_and_Conditions/B_Basic_Data_Types.cpp
@@ -0,0 +1,23 @@
+/*
+CodeForces: Data Type and Conditions - Problem B
+Problem Name: B. Basic Data Types
+Learning: Data Type, Input, Output,
+Description: 
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl << b << endl << c << endl << d << endl << e << endl;
+
+  return 0;
+}
