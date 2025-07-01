ba8cc908d67f963a8d5a50f59adfeef65fc24a23
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-01 23:45:41 +0600

    CodeForces: Data Type and Conditions - Problem G

--- CodeForces/1_Data_Type_and_Conditions/F_Digits_Summation.cpp
+++ CodeForces/1_Data_Type_and_Conditions/F_Digits_Summation.cpp
@@ -1,3 +1,11 @@
+/*
+CodeForces: Data Type and Conditions - Problem G
+Problem Name: G. Summation from 1 to N
+Learning: Data Type, Input, Output,
+Description: Given a number N
+. Print the summation of the numbers that is between 1 and N(inclusive)
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
