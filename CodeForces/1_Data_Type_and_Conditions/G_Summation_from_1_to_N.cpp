f9b3ed2be656296e302a302c41b2b9103c618a28
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 23:14:56 +0600

    CodeForces: Data Type and Conditions - Problem G

---
+++ CodeForces/1_Data_Type_and_Conditions/G_Summation_from_1_to_N.cpp
@@ -0,0 +1,18 @@
+/*
+CodeForces: Data Type and Conditions - Problem G
+Problem Name: G. Summation from 1 to N
+Learning: Data Type, Input, Output,
+Description: sum = n*(n+1)/2
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    long long n;
+    cin >> n;
+
+    cout << n * ( n + 1) / 2 << endl;
+
+  return 0;
+}
