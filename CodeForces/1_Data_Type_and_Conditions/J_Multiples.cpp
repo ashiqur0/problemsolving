caad709d7ddba0f117f80c80f68aac23f9f57f9f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 23:18:12 +0600

    CodeForces: Data Type and Conditions - Problem J

---
+++ CodeForces/1_Data_Type_and_Conditions/J_Multiples.cpp
@@ -0,0 +1,17 @@
+/*
+CodeForces: Data Type and Conditions - Problem J
+Problem Name: J. Multiples
+Learning: Data Type, Input, Output,
+Description: Given two numbers A and B. Print "Multiples" if A is multiple of B or vice versa. Otherwise print "No Multiples".
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    long long a, b;
+    cin >> a >> b;
+    cout << ((a%b==0 || b%a==0)? "Multiples" : "No Multiples") << endl;
+
+  return 0;
+}
