d1bd7d9d63620f51c420a4b276560f6f3620d671
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-07-02 23:16:53 +0600

    CodeForces: Data Type and Conditions - Problem I

---
+++ CodeForces/1_Data_Type_and_Conditions/I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,17 @@
+/*
+CodeForces: Data Type and Conditions - Problem I
+Problem Name: I. Welcome for you with Conditions
+Learning: Data Type, Input, Output,
+Description: Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    long long a, b;
+    cin >> a >> b;
+    cout << (a>=b? "Yes" : "No") << endl;
+
+  return 0;
+}
