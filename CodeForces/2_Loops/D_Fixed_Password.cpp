7e1b843571205d31ab3898657b4e2cfc694ef30d
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-08-17 23:43:26 +0600

    CodeForces Problem: correct Password

---
+++ CodeForces/2_Loops/D_Fixed_Password.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int n;
+    while (cin >> n) {
+        if (n == 1999) {
+            cout << "Correct" << endl;
+            break;
+        }
+        cout << "Wrong" << endl;
+    }
+
+  return 0;
+}
